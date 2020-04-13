a04fad909beb2ca0b1feb41e63cadcdc106625da
Eltom777 <lthomas.flynn@gmail.com>
2020-04-13 17:06:21 -0400

    Completed VMM

--- VMM.java
+++ VMM.java
@@ -4,45 +4,245 @@ import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.File;
+import java.util.ArrayList;
 
 public class VMM extends Thread {
 
+	private static int memSize;
+	private static mem[] mainMemory;
+	private static int counter = 0;
+
     public static void main (String args[]) throws FileNotFoundException{
 
+		//configure main memory into an array
         String[] memconfig = readFile("memconfig.txt").split("\\s+");
-        int memSize = Integer.parseInt(memconfig[0]);
+		memSize = Integer.parseInt(memconfig[0]);
+		mainMemory = new mem[memSize];
+		
+		for(int i = 0; i < memSize; i++) {
+			System.out.println(mainMemory[i]);
+		}
+		
 
-        String[] commandFile = readFile("command.txt").split("\n)");
+		
+		//create new vm.txt file for virtual memory
+		//File f = new File("./vm.txt");// variable id | variable value | age
+		
+		//read form command file
+        String[] commandFile = readFile("command.txt").split("\n");
         
         VMM v = new VMM();
 		
-		
+        
 		for(String input: commandFile){
 			String[] commands = input.split(" ");
 			
-			//
 			switch (commands[0].toLowerCase()) {
-				case "store": v.memStore(Integer.parseInt(commands[1]),Integer.parseInt(commands[2]));
-				case "release": v.memFree(Integer.parseInt(commands[1]));
-				case "lookup": v.memLookup(Integer.parseInt(commands[1]));
+				case "store": 
+					System.out.println(v.memStore(Integer.parseInt(commands[1]),Integer.parseInt(commands[2])));
+					break;
+				case "release": 
+					v.memFree(Integer.parseInt(commands[1]));
+					break;
+				case "lookup": 
+					v.memLookup(Integer.parseInt(commands[1]));
+					break;
 				default: System.out.println("Command "+commands[0]+" was not recognized");
 			}
 		}
-
     }
     
     //API
-    public void memStore(int variableId, int value) {
-    	
+    public int memStore(int variableId, int value) {
+
+		//if already exist in MM
+			//overwrite in MM
+		//else if already exist in VM
+			//if write to MM if free slot
+			//else (no avaliable spot) swap oldest from MM to VM and write to MM
+		//else (it doesn't exist)
+			//swap and insert
+			//swapped value is stored in VM
+		
+		for(int i = 0; i < memSize; i++) {// exist in MM
+			if(mainMemory[i] != null) {
+				if(mainMemory[i].getID() == variableId) {
+					mainMemory[i].setVal(value);
+					return mainMemory[i].getVal();
+				}
+			}
+		}
+
+		ArrayList<mem> tempMem1 = fromVM();
+		ArrayList<mem> tempMem = tempMem1;
+		int index;
+		for (mem m: tempMem1) {
+			if(m.getID() == variableId) {
+				if(counter < memSize) { //Free spot avaliable
+					index = emptyFrame();
+					mainMemory[index] = m; //insert lookup in MM
+					tempMem.remove(tempMem1.indexOf(m)); //remove lookup from disk
+					mainMemory[index].setVal(value); // set new value
+					counter ++;
+					toVM(tempMem);
+					return mainMemory[index].getVal();
+				}
+				else{ //must swap
+					index = oldestPage();
+					mem swapMem = new mem(mainMemory[index]); //stores page to be swapped
+					mainMemory[index] = m; //overwrites page in MM
+					mainMemory[index].setVal(value); // set new value
+					tempMem.add(swapMem); //add swapped page to VM arraylist
+					tempMem.remove(tempMem1.indexOf(m)); // remove lookup from disk
+					toVM(tempMem); //writes back to disk
+					System.out.println("Time " + scheduler.time + " Memory Manager, Swap: Variable " + swapMem.getID() +" with Variable " + m.getID());
+					return mainMemory[index].getVal();
+				}
+			}
+		}
+
+		mem newMem = new mem(variableId, value, scheduler.time); // doesn't exist 
+
+		if(counter < memSize){
+			System.out.println("here");
+			index = emptyFrame();
+			mainMemory[index] = newMem;
+			counter++;
+			return mainMemory[index].getVal();
+		}
+		else{
+			index = oldestPage();
+			mem swapMem = new mem(mainMemory[index]); //stores page to be swapped
+			mainMemory[index] = newMem; //overwrites page in MM
+			tempMem.add(swapMem); //add swapped page to VM arraylist
+			toVM(tempMem); //writes back to disk
+			System.out.println("Time " + scheduler.time + " Memory Manager, Swap: Variable " + swapMem.getID() +" with Variable " + newMem.getID());
+			return mainMemory[index].getVal();
+		}
     }
     
-    public void memFree(int variableId) {
-    	
+	public int memFree(int variableId) { //delete
+
+		//if in MM
+		//if in VM
+			//swap to MM then release
+
+		for(int i = 0; i < memSize; i++){
+			if(mainMemory[i].getID() == variableId){
+				mainMemory[i] = null;
+				counter--;
+				return 1; //returns 1 if successful
+			}
+		}
+
+		return -1;
     }
     
-    public void memLookup(int variableId) {
-    	
-    }
+    public int memLookup(int variableId) {
+
+		//go through main mem
+		//if not present then fromMem()
+		//go through virtual mem, pop from arraylist 
+		//if present swap into main mem and output
+		//return swapped string toMem()
+
+		for(int i = 0; i < memSize; i++){
+			if(mainMemory[i] != null) {
+				if(mainMemory[i].getID() == variableId){
+					return mainMemory[i].getVal();
+				}
+			}
+		}
+
+		
+		ArrayList<mem> tempMem1 = fromVM();
+		ArrayList<mem> tempMem = tempMem1;
+		int index;
+		for (mem m: tempMem1) {
+			if(m.getID() == variableId) { 
+				if(counter < memSize) { //Free spot avaliable
+					index = emptyFrame();
+					mainMemory[index] = m; //insert lookup in MM
+					tempMem.remove(tempMem1.indexOf(m)); //remove lookup from disk
+					counter++;
+					toVM(tempMem);
+					return mainMemory[index].getVal();
+				}
+				else{ //must swap
+					index = oldestPage();
+					mem swapMem = new mem(mainMemory[index]);//stores page to be swapped
+					mainMemory[index] = m;//overwrites page in MM
+					tempMem.add(swapMem);//add faulted page to VM arraylist
+					tempMem.remove(tempMem1.indexOf(m)); // remove lookup from disk
+					toVM(tempMem);//writes back to disk
+					System.out.println("Time " + scheduler.time + " Memory Manager, Swap: Variable " + swapMem.getID() +" with Variable " + m.getID());
+					return mainMemory[index].getVal();
+				}
+			}
+		}
+	
+		return -1;
+	}
+	
+	//make a function to pull vm.txt into an mem array
+	public ArrayList<mem> fromVM(){ //string converts into array of mem
+		String[] fromDisk = readFile("vm.txt").split("\\s+");
+		ArrayList<mem> mems = new ArrayList<mem>();
+		for (int i = 0; i < fromDisk.length/3; i++){
+			mems.add(new mem(
+				Integer.parseInt(fromDisk[3 * i]),
+				Integer.parseInt(fromDisk[3 * i + 1]),
+				Integer.parseInt(fromDisk[3 * i + 2])));
+		}
+		return mems;
+	}
+
+	//make a funtion that puts mem array into vm.txt
+	public void toVM (ArrayList<mem> toDisk){ //converts array of mem into string
+		
+		//split every 3 entry
+		//3 entry per row
+		//1st entry is Id
+		//2nd entry is the value
+		//3rd entry is the age
+
+		try {
+			PrintWriter pw = new PrintWriter(new File("vm.txt")); //erases old vm.txt
+			for (mem m: toDisk){
+				pw.println(m.getID() + " " + m.getVal() + " " + m.getAge());
+			}
+
+			pw.flush();
+			pw.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+	}
+
+	
+	
+	//find the index of the oldestPage (only call if mainMemory is full)
+	public int oldestPage(){ 
+		int index = 0;
+		for(int i = 1; i < memSize; i++){
+			
+			if(mainMemory[i].getAge() > mainMemory[i-1].getAge()) index = i;
+			
+		} 
+		return index;
+		// no avaliable slots
+	}
+
+	public int emptyFrame(){
+		for(int i = 0; i < memSize; i++){
+			if(mainMemory[i] == null){
+				return i;
+			}
+		}
+		return -1;
+	}
 
     private static String readFile(String name) {
 		try {
@@ -59,5 +259,6 @@ public class VMM extends Thread {
 		} catch (Exception e) {
 			return null;
 		}
-    }
+	}
+	
 }
